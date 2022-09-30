528c044158767227a3a489b6f68246257f5c52fb
xernona <xeraldey@gmail.com>
2022-09-30 16:03:32 +0700

    Create MsMenuImageUrlNew.cs

---
+++ BercaCafe_API/Models/MsMenuImageUrlNew.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BercaCafe_API.Models
+{
+    public partial class MsMenuImageUrlNew
+    {
+        public int MenuId { get; set; }
+        public string LinkUrl { get; set; }
+
+        public virtual MsUdc Menu { get; set; }
+    }
+}
